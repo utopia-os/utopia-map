ff31182b3452d812d357bb0359a24c89b504a5c5
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2024-11-11 16:26:40 +0100

    polyfill node functions

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 // vite.config.js
 import { VitePWA } from 'vite-plugin-pwa'
 
@@ -10,6 +11,7 @@ export default defineConfig({
     port: 5174
   },
   plugins: [
+    nodePolyfills(),
     react()
   ]
 })
