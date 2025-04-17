c41e8a65a15d929dc81d4b0084251e98fb98af58
Anton Tranelis <mail@antontranelis.de>
2025-04-17 13:27:17 +0100

    get open collective api key from .env

--- src/pages/MapContainer.tsx
+++ src/pages/MapContainer.tsx
@@ -80,6 +80,8 @@ function MapContainer({ layers, map }: { layers: LayerProps[]; map: any }) {
         showLayerControl={map.show_layer_control}
         showGratitudeControl={map.show_gratitude_control}
         donationWidget={map.donation_widget}
+        showThemeControl={map.show_theme_control}
+        defaultTheme={map.default_theme}
       >
         {layers &&
           apis &&
@@ -142,6 +144,11 @@ function MapContainer({ layers, map }: { layers: LayerProps[]; map: any }) {
             </Layer>
           ))}
       </UtopiaMap>
+      <p>123</p>
+      <p>123</p>
+      <p>123</p>
+      <p>123</p>
+      <p>123</p>
     </>
   )
 }
