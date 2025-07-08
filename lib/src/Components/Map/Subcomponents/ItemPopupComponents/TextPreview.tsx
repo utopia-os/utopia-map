66231a50226096d41997b3939b2671f44205a32b
Anton Tranelis <mail@antontranelis.de>
2025-07-08 16:07:59 +0200

    no truncate

--- lib/src/Components/Map/Subcomponents/ItemPopupComponents/TextPreview.tsx
+++ lib/src/Components/Map/Subcomponents/ItemPopupComponents/TextPreview.tsx
@@ -1,23 +1,19 @@
-import truncate from 'markdown-truncate'
-
 import { RichTextEditor } from '#components/Input/RichTextEditor/RichTextEditor'
 import { fixUrls, mailRegex } from '#utils/ReplaceURLs'
 
 import type { Item } from '#types/Item'
 
 export const TextPreview = ({ item }: { item: Item }) => {
+  let replacedText = ''
+
   if (!item.text) return null
-  else {
-    let replacedText = truncate(item.text, { limit: 100, ellipsis: true }) as string
+  else replacedText = fixUrls(item.text)
 
-    replacedText = fixUrls(item.text)
-
-    if (replacedText) {
-      replacedText = replacedText.replace(mailRegex, (url) => {
-        return `[${url}](mailto:${url})`
-      })
-    }
-
-    return <RichTextEditor defaultValue={replacedText} readOnly={true} />
+  if (replacedText) {
+    replacedText = replacedText.replace(mailRegex, (url) => {
+      return `[${url}](mailto:${url})`
+    })
   }
+
+  return <RichTextEditor defaultValue={replacedText} readOnly={true} />
 }
