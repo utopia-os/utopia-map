e94ecebf453cfb2c8804ede747cc735e5dc56bf1
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-08-21 10:19:21 +0200

    delete manual seed files & script

--- backend/directus-config/manual/seed.sh
+++ backend/directus-config/manual/seed.sh
@@ -3,16 +3,8 @@
 # base setup
 SCRIPT_PATH=$(realpath $0)
 SCRIPT_DIR=$(dirname $SCRIPT_PATH)
-ROOT_DIR=$SCRIPT_DIR/../../..
 
-DATA_DIR=$ROOT_DIR/data
-DATA_UPLOADS_DIR=$DATA_DIR/uploads
-
-SEED_FILES_DIR=$SCRIPT_DIR/files
 SEED_SQL_DIR=$SCRIPT_DIR/sql
-
-# copy files
-cp $SEED_FILES_DIR/* $DATA_UPLOADS_DIR/
 
 # apply database updates
 for filename in $SEED_SQL_DIR/*.sql; do
