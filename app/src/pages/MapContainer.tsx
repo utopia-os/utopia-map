e8efb7e23406c446c939dd68fb33742da7c02296
Anton Tranelis <mail@antontranelis.de>
2025-08-25 20:13:09 +0200

    fix linting

--- app/src/pages/MapContainer.tsx
+++ app/src/pages/MapContainer.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unnecessary-condition */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable import/no-relative-parent-imports */
 /* eslint-disable array-callback-return */
 /* eslint-disable new-cap */
