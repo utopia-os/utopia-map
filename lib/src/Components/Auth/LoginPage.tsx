245276836293811889e7bdce34be4fb1c1234039
Anton Tranelis <mail@antontranelis.de>
2025-08-27 23:42:40 +0200

    fixed linting

--- lib/src/Components/Auth/LoginPage.tsx
+++ lib/src/Components/Auth/LoginPage.tsx
@@ -144,7 +144,10 @@ export function LoginPage({ inviteApi, showRequestPassword }: Props) {
                   d='M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
                 ></path>
               </svg>
-              <span>The map requires an additional password. If you don't have it yet, you can request one. </span>
+              <span>
+                The map requires an additional password. If you don&apos;t have it yet, you can request
+                one.{' '}
+              </span>
             </div>
             <Link to='/reset-password' className='tw:w-full'>
               <button className='tw:btn tw:btn-primary tw:btn-block'>{'Request Password'}</button>
