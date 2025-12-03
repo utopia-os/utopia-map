f6605afa28a4440074e1e61e3cb677bd0b70f871
mahula <lenzmath@posteo.de>
2025-12-03 19:56:26 +0100

    linting: remove comment for deprecated rule

--- lib/src/Components/Map/hooks/useLeafletRefs.tsx
+++ lib/src/Components/Map/hooks/useLeafletRefs.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable react/prop-types */
-/* eslint-disable @typescript-eslint/ban-types */
 import { useCallback, useReducer, createContext, useContext } from 'react'
 
 import type { Item } from '#types/Item'
